df80c2040dafaba40d267ae88c6baeafffe8fad3
Ariel Lindemann <ariel.lindemann@tpt.de>
2024-07-25 17:49:10 +0200

    added clang-format

--- lua/ariel/plugins/lspconfig.lua
+++ lua/ariel/plugins/lspconfig.lua
@@ -9,6 +9,10 @@ return {
       initialization_options = {
         fallback_flags = { '-std=c++17' },
       },
+      on_attach = function(client, bufnr)
+        -- Formatting on save
+        vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({async = true})]]
+      end
     })
   end
 }
