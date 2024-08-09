1f1e5a0f8a0c66f773aff61fd0b9b12570bd1bb8
Ariel Lindemann <ariel.lindemann@tpt.de>
2024-08-09 11:54:23 +0200

    added mason

--- lua/ariel/plugins/lspconfig.lua
+++ lua/ariel/plugins/lspconfig.lua
@@ -1,14 +1,3 @@
 return {
   "neovim/nvim-lspconfig",
-
-  config = function()
-    local lspconfig = require('lspconfig')
-    lspconfig.clangd.setup({
-      name = 'clangd',
-      cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
-      initialization_options = {
-        fallback_flags = { '-std=c++17' },
-      },
-    })
-  end
 }
