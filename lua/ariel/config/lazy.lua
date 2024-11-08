eae2ac44a3839f2c80400c8b6747a824beb0e96e
Ariel Lindemann <ariel.lindemann@tpt.de>
2024-11-08 13:30:49 +0100

    disbled hererocks

--- lua/ariel/config/lazy.lua
+++ lua/ariel/config/lazy.lua
@@ -26,4 +26,6 @@ require("lazy").setup({
   -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+
+  rocks = { enabled = false },
 })
