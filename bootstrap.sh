12a8b75ad254d99e2345cda3237164c39cb9d890
Ariel Lindemann <ariel.lindemann@gmail.com>
2025-01-08 23:22:58 +0100

    add download function and install curl beforehand

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+download_nvim() {
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	rm nvim-linux64.tar.gz
+}
 
 add_to_path() {
 
@@ -36,11 +38,12 @@ add_to_path() {
 
 add_to_path
 
-printf "Installing dependencies ..."
+printf "Installing dependencies ...\n"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	sudo apt update
 	sudo apt install -y \
+		curl \
 		gcc \
 		ripgrep
 		nodejs \
@@ -53,6 +56,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 else
 	echo "Automatic dependency installation not supported for $OSTYPE. Please install them manually"
 fi
+
+download_nvim
 
 printf "\nChecking installation ...\n\n"
 
