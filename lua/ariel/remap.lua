9416ec1dabad22e031a64361f9c6d68a2f0a600d
Ariel Lindemann <ariel.lindemann@gmail.com>
2025-01-18 14:45:16 +0100

    added remaps for moving, indenting and LSP

--- lua/ariel/remap.lua
+++ lua/ariel/remap.lua
@@ -1,5 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<C-n><C-m>", ':set invrelativenumber<CR>', { noremap = true })
-vim.keymap.set("n", "<C-n><C-n>", ':set invnumber<CR>', { noremap = true })
+vim.keymap.set("n", "<C-n><C-m>", ":set invrelativenumber<CR>", { noremap = true })
+vim.keymap.set("n", "<C-n><C-n>", ":set invnumber<CR>", { noremap = true })
+
+-- Move selected block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Stay in visual mode after indentation
+vim.keymap.set("v", "<", "<gv", { noremap = true })
+vim.keymap.set("v", ">", ">gv", { noremap = true })
+
+-- LSP
+vim.keymap.set("n", "<leader>gd", ":lua vim.lsp.buf.definition()<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gi", ":lua vim.lsp.buf.implementation()<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gr", ":lua vim.lsp.buf.references()<CR>", { noremap = true })
